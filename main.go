ca25e3fd6904663d7dd63857102267e53251da8f
Panagiotis Siatras <ps@primetech.hr>
2017-12-21 01:04:39 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+func main() {
+	log.SetFlags(log.LUTC | log.Lmicroseconds)
+	log.SetPrefix(filepath.Base(os.Args[0] + ": "))
+
+	layouts, err := validLayouts()
+	if err != nil {
+		log.Printf("cannot retrieve valid layouts: %s", err)
+		return
+	} else if len(layouts) == 0 {
+		return
+	}
+
+	var rotation []string
+	for _, l := range os.Args[1:] {
+		i := sort.SearchStrings(layouts, l)
+
+		if !(i < len(layouts) && layouts[i] == l) {
+			// missing
+			continue
+		}
+
+		rotation = append(rotation, l)
+	}
+	sort.Strings(rotation)
+
+	if len(rotation) < 2 {
+		// nothing to rotate
+		return
+	}
+
+	current, err := currentLayout()
+	if err != nil {
+		log.Printf("cannot get current layouts: %s", err)
+	}
+
+	index := sort.SearchStrings(rotation, current)
+	if index >= len(rotation) || rotation[index] != current {
+		// missing
+		return
+	}
+
+	if index == len(rotation)-1 {
+		index = 0
+	} else {
+		index++
+	}
+
+	newLayout := rotation[index]
+	if err := setLayout(newLayout); err != nil {
+		log.Printf("cannot switch to %q: %s", newLayout, err)
+	}
+}
+
+func validLayouts() ([]string, error) {
+	buf := &bytes.Buffer{}
+
+	cmd := exec.Command("localectl", "--no-pager", "list-x11-keymap-layouts")
+	cmd.Stdout = buf
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+
+	var ret []string
+
+	for {
+		token, err := buf.ReadString('\n')
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, strings.TrimSpace(token))
+	}
+
+	sort.Strings(ret)
+	return ret, nil
+}
+
+func currentLayout() (string, error) {
+	const prefix = "layout:"
+
+	buf := &bytes.Buffer{}
+
+	cmd := exec.Command("setxkbmap", "-query")
+	cmd.Stdout = buf
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	ret := ""
+
+	for {
+		line, err := buf.ReadString('\n')
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return "", err
+		}
+
+		if !strings.HasPrefix(line, prefix) {
+			continue
+		}
+
+		tokens := strings.Split(strings.TrimSpace(line[len(prefix):]), ",")
+		if len(tokens) > 0 {
+			ret = tokens[0]
+			break
+		}
+	}
+
+	if ret == "" {
+		return ret, errors.New("no current layout determined")
+	}
+
+	return ret, nil
+}
+
+func setLayout(l string) error {
+	return exec.Command("setxkbmap", "-layout", l).Run()
+}
